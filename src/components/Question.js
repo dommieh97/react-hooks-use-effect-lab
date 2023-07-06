c787d145ce61620d7a6b5dcf0591de6691ece4fd
Domenique <domogenesis77@gmail.com>
2023-07-06 16:07:33 -0500

    pass

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
 
-  function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
-    onAnswered(isCorrect);
-  }
+  // add useEffect code
+  useEffect(() => {
+    let timeoutId = null;
+
+    const intervalId = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => {
+        if (prevTimeRemaining === 1) {
+          clearInterval(intervalId);
+          clearTimeout(timeoutId);
+          onAnswered(false);
+        }
+        return prevTimeRemaining - 1;
+      });
+    }, 1000);
+
+    timeoutId = setTimeout(() => {
+      clearInterval(intervalId);
+      onAnswered(true); 
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+      clearTimeout(timeoutId);
+    };
+  }, [onAnswered]);
 
   const { id, prompt, answers, correctIndex } = question;
 
